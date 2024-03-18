cf53ef27d3aa296665e72a9d295777bcc3e8c00a
Harshit Saini <harshit00saini00199@gmail.com>
2024-03-19 01:40:18 +0530

    Sparse Matrix completed !!!!

--- 01AnjalMaam/SomeThingNew/sparce_matrix.cpp
+++ 01AnjalMaam/SomeThingNew/sparce_matrix.cpp
@@ -1,7 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class List;
+
+int using_array();
+int using_list();
+
 int main()
+{
+
+    // using_array();
+    using_list();
+
+    return 0;
+}
+class List
+{
+    struct Node
+    {
+        int row;
+        int col;
+        int data;
+        Node *next;
+    };
+    int length;
+
+public:
+    Node *start;
+    List();
+    void add_at_end(int, int, int);
+    void print();
+};
+List::List()
+{
+    this->length = 0;
+    this->start = nullptr;
+}
+
+void List::add_at_end(int row, int col, int data)
+{
+
+    Node *ptr, *temp;
+
+    temp = new Node;
+    temp->row = row;
+    temp->col = col;
+    temp->data = data;
+    temp->next = nullptr;
+    if (start == nullptr)
+    {
+        start = temp;
+        this->length++;
+        return;
+    }
+
+    ptr = start;
+    while (ptr->next != nullptr)
+    {
+        ptr = ptr->next;
+    }
+
+    ptr->next = temp;
+    this->length++;
+}
+void List::print()
+{
+    Node *ptr = start;
+    cout << "Row\tCol\tData\n";
+    while (ptr != nullptr)
+    {
+        cout << ptr->row << "\t" << ptr->col << "\t" << ptr->data << "\n";
+        ptr = ptr->next;
+    }
+
+    cout << endl;
+}
+
+int using_array()
 {
 
     system("cls");
@@ -53,6 +128,34 @@ int main()
         }
         cout << endl;
     }
+}
+int using_list()
+{
+    system("cls");
+    int r = 4, c = 5;
+    int arr[r][c] =
+        {
+            {0, 0, 3, 0, 4},
+            {0, 0, 5, 7, 0},
+            {0, 0, 0, 0, 0},
+            {0, 2, 6, 0, 0}};
 
-    return 0;
+    int size = 0;
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            if (arr[i][j] != 0)
+                size++;
+
+    int vals = 0;
+    List l1;
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            if (arr[i][j] != 0)
+            {
+                l1.add_at_end(i, j, arr[i][j]);
+                vals++;
+            }
+
+    l1.print();
 }
